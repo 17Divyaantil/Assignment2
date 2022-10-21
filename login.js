4707fbd8adf5e5f15cbff595eb3a1b44c3892f3b
Vaishali Wagh <vkwagh1208@gmail.com>
2022-10-21 22:12:16 +0530

    login functionality_added

--- login.js
+++ login.js
@@ -1,2 +1,16 @@
 1 let reg_email ="abc@gmail.com";
 2 let reg_pass = "12345";
+
+  let enter_email ="abc@gmail.com";
+
+  let enter_pass = "12345";
+
+  if(reg_email === enter_email && reg_pass === enter_pass) {
+
+      console.log("Login successful");
+ 
+   }
+   else {
+
+      console.log("Login failed");
+   }
